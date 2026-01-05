e04d5976002a37d1e29ae205f9472b9f95432e26
Pawan Bhadana <2002pawanbhadana@gmail.com>
2026-01-05 20:08:29 +0530

    Implement paragraph-based chunking with overlap for RAG and added gitignore

---
+++ chunking.py
@@ -0,0 +1,28 @@
+with open('PROJECT_NOTES.txt', 'r') as file:
+    text = file.read()
+
+# print(text[:50])
+
+chunks=text.split("\n\n")
+
+# print(chunks[:10])
+
+overlap=30
+size=150
+new_chunks=[]
+
+for chunk in chunks:
+    words=chunk.split()
+    n=len(words)
+    if(n==0):continue
+    prev=""
+    for i in range(len(words)//size +1):
+        temp=words[i*size:min(n,(i+1)*size)]
+        new_chunks.append(prev + " " + " ".join(temp))
+        prev=" ".join(temp[-overlap:])
+
+print(new_chunks[:10])
+
+
+
+
